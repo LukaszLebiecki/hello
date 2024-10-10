9f0f6e56079a610dd6a6d56af23fe6100f5fca92
Łukasz Lebiecki <lukasz.lebiecki@gmail.com>
2024-10-10 22:20:02 +0200

    lesson variables

---
+++ src/Variables/Exercise1.java
@@ -0,0 +1,17 @@
+package Variables;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        String brand = "Mazda";
+        String model = "3";
+        int year = 2018;
+        double price = 93200.40;
+
+        System.out.println("Marka: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Rocznik: " + year);
+        System.out.println("Cena: " + price + "zł");
+
+
+    }
+}
