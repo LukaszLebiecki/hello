03d23fc97f7ba6f428c076c664f9dad5e8ef7c7d
LukaszLebiecki <lukasz.lebiecki@gmail.com>
2024-10-16 20:46:21 +0200

    added class game HeadsOrTails.java

---
+++ src/Data/Exercise2/HeadsOrTails.java
@@ -0,0 +1,25 @@
+package Data.Exercise2;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class HeadsOrTails {
+    public static void main(String[] args) {
+
+        final int obverse = 1;
+        final int reverse = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wbierz orzeł czy reszka (1 - orzeł; 0 - reszka): ");
+        int userChoice = scanner.nextInt();
+        Random random = new Random();
+        int flipResult = random.nextInt(2);
+
+
+        if(userChoice == flipResult) {
+            System.out.println("Brawo wygrałeś");
+        } else {
+            System.out.println("Przegrałeś");
+        }
+    }
+}
