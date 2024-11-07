3b720e49d41ec6de558b3a699949547f437ba335
Łukasz Lebiecki <lukasz.lebiecki@gmail.com>
2024-11-07 21:56:57 +0100

    Arrays example1

--- src/Array/Exercise1/PersonDatabase.java
+++ src/Array/Exercise1/PersonDatabase.java
@@ -1,35 +1,50 @@
 package Array.Exercise1;
 
+import java.util.Arrays;
+
 public class PersonDatabase {
     private final int INITIAL_CAPACITY = 1;
     private Person[] people = new Person[INITIAL_CAPACITY];
-    private int emptyPlace = 1;
+    private int emptyPlace = 0;
+    private int numberOfPerson = 0;
 
     public void add(Person p) {
-        // TODO rozszerzyć tablice
+        if (people.length <= numberOfPerson) {
+            people = Arrays.copyOf(people, people.length + 1);
+        }
         people[emptyPlace] = p;
+        numberOfPerson++;
         emptyPlace++;
     }
 
     public void remove(Person p) {
-        // TODO
-        for (Person person : people) {
-            if (p.equals(person)) {
-
+        Person[] copy = new Person[people.length];
+        for (int i = 0; i < people.length; i++) {
+            if(p.equals(people[i])) {
+                System.arraycopy(people, 0, copy, 0, i);
+                System.arraycopy(people, i+1, copy, i, people.length - (i+1));
             }
         }
+        numberOfPerson--;
+        people = copy;
     }
 
+
     public Person get(int index) {
-        return null;
+        return people[index];
     }
 
     public int size() {
-        return 0;
+        return numberOfPerson;
     }
 
     @Override
     public String toString() {
-        return super.toString();
+        StringBuilder text = new StringBuilder();
+        for (Person person : people) {
+            text.append(person);
+            text.append("\n");
+        }
+        return text.toString();
     }
 }
--- src/Array/Exercise1/Test.java
+++ src/Array/Exercise1/Test.java
@@ -3,5 +3,25 @@ package Array.Exercise1;
 public class Test {
     public static void main(String[] args) {
 
+        PersonDatabase personDatabase = new PersonDatabase();
+
+        personDatabase.add(new Person("Nikodem", "Lebiecki", "123456789"));
+        personDatabase.add(new Person("Nikodem2", "Lebiecki2", "123456789"));
+        personDatabase.add(new Person("Nikodem3", "Lebiecki3", "123456789"));
+        personDatabase.add(new Person("Nikodem4", "Lebiecki4", "123456789"));
+        personDatabase.add(new Person("Nikodem5", "Lebiecki5", "123456789"));
+        personDatabase.add(new Person("Nikodem6", "Lebiecki6", "123456789"));
+
+        System.out.println(personDatabase);
+        System.out.println(personDatabase.size());
+        System.out.println(personDatabase.get(3));
+
+        System.out.println("=============");
+
+        personDatabase.remove(new Person("Nikodem3", "Lebiecki3", "123456789"));
+        System.out.println(personDatabase);
+        System.out.println(personDatabase.size());
+        System.out.println(personDatabase.get(3));
+
     }
 }
